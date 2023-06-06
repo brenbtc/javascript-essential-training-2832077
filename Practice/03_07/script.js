92b4647294ec3deabf9d4831ee2788e4a70623b9
brenbtc <yanohb@yahoo.com>
2023-06-06 01:43:40 -0600

    JS Practice

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,18 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+
+const backpack = {
+    laptop: 'MacBook Pro 17',
+    notebook: 'JavaScript Mastery',
+    pencilBox: {
+        coloredSharpie1: 'Black',
+        coloredSharpie2: 'Yellow',
+        coloredSharpie3: 'Green',
+        coloredSharpie4: 'Red',
+    }
+}
+
+console.log('Brens Laptop: ', backpack.laptop)
+console.log("Bren's Pencilbox: ", backpack.pencilBox)
